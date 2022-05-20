a9af91a89f0e29fca46b0aab460f39d7fe6d75c5
MaksimSimanskiy <78483044+MaksimSimanskiy@users.noreply.github.com>
2022-05-20 13:56:29 +0300

    Add files via upload

--- task1_1.py
+++ task1_1.py
@@ -2,59 +2,57 @@ from threading import Thread
 import time
 import random
 from queue import Queue
-import math
 
 """
 Разработать приложение, в котором выполнить решение вычислительной задачи
 (например, задачи из области физики, экономики, математики, статистики и т. д.) с помощью
 паттерна “Производитель-Потребитель”, условие которой предварительно необходимо
-согласовать с преподавателем.
+согласовать с преподавателем
 """
-
+"""
+Взята задача обработок  посылок на почте.
+"""
 queue = Queue()
 
 
-class Shooter():
-    def __init__(self, p, name):
-        self.p = p
-        self.i = 1
-        self.name = name
+class Package():
+    def __init__(self, weight, tip):
+        self.id = random.randint(1, 1000)
+        self.weight = weight
+        self.tip = tip
 
-    def prb(self):
-        p = math.pow(self.p, self.i)
-        self.i += 1
-        return p
+    def handling(self):
+        if self.weight >= 1000:
+            print("Большой пакет, цена отправки - ", round(self.weight * 0.7, 2))
+        else:
+            print("Мелкий пакет,  цена отправки - ", round(self.weight * 0.8, 2))
+        print("--------------")
+
 
 
 class ProducerThread(Thread):
     def run(self):
-        count_shoots = 0
-        while count_shoots < 20:
-            i = random.randint(0, 5)
-            queue.put(shoot[i])
-            print(f"Стреляет {shoot[i].name}  -  {shoot[i].i} раз \n")
+        counter = 0
+        while counter < 20:
+            queue.put(parsels[counter])
+            print(f"Получена посылка, id - {parsels[counter].id} \n")
             time.sleep(0.1)
-            count_shoots += 1
+            counter += 1
 
 
 class ConsumerThread(Thread):
     def run(self):
-        shoot_consumed = 0
-        while shoot_consumed < 20:
-            num = queue.get()
+        parsels_consumed = 0
+        while parsels_consumed < 20:
+            obj = queue.get()
             queue.task_done()
-            print (f"Вероятность попадания {num.i} выстрелов подряд -  {num.prb()} \n")
+            print(f"Посылка {obj.tip}")
+            obj.handling()
             time.sleep(0.1)
-            shoot_consumed += 1
+            parsels_consumed += 1
 
 
 if __name__ == '__main__':
-    str_1 = Shooter(0.5, "Иван ")
-    str_2 = Shooter(0.79, "Сергей ")
-    str_3 = Shooter(0.57, "Дима ")
-    str_4 = Shooter(0.45, "Павел ")
-    str_5 = Shooter(0.66, "Миша ")
-    str_6 = Shooter(0.9, "Егор ")
-    shoot = [str_1, str_2, str_3, str_4, str_5, str_6]
+    parsels = [Package(random.randint(20, 2000), random.choice(['с наложным платежем', 'заказная', 'простая'])) for i in range(20)]
     ProducerThread().start()
     ConsumerThread().start()
